4df03a84cc76228b65e4424c1033372e5f126628
Maarten Hogeveen <mhogeveen94@gmail.com>
2024-01-31 20:55:38 +0100

    feat: add rest of term colors

--- colorize/colorize.go
+++ colorize/colorize.go
@@ -5,17 +5,25 @@ import "fmt"
 type ColorCode string
 
 const (
-	RED   = "\033[0;31m"
-	GREEN = "\033[0;32m"
-	BLUE  = "\033[0;33m"
-	NONE  = "\033[0m"
+	RED    = "\033[0;31m"
+	GREEN  = "\033[0;32m"
+	YELLOW = "\033[0;33m"
+	BLUE   = "\033[0;34m"
+	PURPLE = "\033[0;35m"
+	CYAN   = "\033[0;36m"
+	GRAY   = "\033[0;37m"
+	NONE   = "\033[0m"
 )
 
 var colorCodes = map[string]ColorCode{
-	"red":   RED,
-	"green": GREEN,
-	"blue":  BLUE,
-	"none":  NONE,
+	"red":    RED,
+	"green":  GREEN,
+	"yellow": YELLOW,
+	"blue":   BLUE,
+	"purple": PURPLE,
+	"cyan":   CYAN,
+	"gray":   GRAY,
+	"none":   NONE,
 }
 
 func Colorize(s string, c string) string {
